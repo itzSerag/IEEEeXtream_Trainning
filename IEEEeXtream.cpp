ea1cc37d62083d229aa81316e888df756bd79d13
itzSerag <serag.eldien.mahmoud@gmail.com>
2023-09-21 12:59:55 +0300

    Add project files.

---
+++ IEEEeXtream.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+int cubic(int x , int y(int z)) {
+	return y(x) * x;
+}
+
+int square(int x) {
+	return x * x;
+}
+
+
+
+
+int main() { // executes first 
+	// full control of the variables
+
+	//init
+	int c = 10 + 100 ;
+	string cc = to_string(c);
+
+	//logic
+	
+
+	cout << cc << endl;
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
